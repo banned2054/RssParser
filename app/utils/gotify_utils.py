50103683360d70809be58a7ecd765a6598a69dd5
banned2054 <banned2054@163.com>
2025-07-09 16:34:03 +0800

    fix: 修复gotify的问题

--- app/utils/gotify_utils.py
+++ app/utils/gotify_utils.py
@@ -1,10 +1,8 @@
 from gotify import AsyncGotify
 
-gotify = AsyncGotify(
-        base_url = "REDACTED_GOTIFY_URL",
-        app_token = "REDACTED_GOTIFY_TOKEN_1",
-)
-
 
 async def send_gotify(message: str) :
+    gotify = AsyncGotify(
+            base_url = "REDACTED_GOTIFY_URL_LOCAL",
+            app_token = "REDACTED_GOTIFY_TOKEN_1", )
     await gotify.create_message(message)
